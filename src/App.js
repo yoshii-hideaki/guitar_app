a0cfb2f67ca42b09ff002f70e005568544baa1f1
吉井秀旭 <yoshiihideaki@yoshiishuuasahinoMacBook-Air.local>
2025-04-08 18:51:27 +0900

    Pages ver

--- src/App.js
+++ src/App.js
@@ -1,38 +1,67 @@
-import ChordDisplay from "./components/chord_display"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Metronome from "./components/metronome"
+import ChordDisplay from "./components/chord_display"
+import ChordGallery from "./components/ChordGallery"
 import { BpmProvider } from "./components/BpmContext"
+import { Link } from "react-router-dom"
+
+function Home() {
+  return (
+    <>
+      <ChordDisplay />
+      <Metronome />
+    </>
+  )
+}
 
 function App() {
   return (
     <BpmProvider>
-      <div
-        style={{
-          backgroundColor: "#121212",
-          minHeight: "100vh",
-          padding: "40px 20px",
-          fontFamily: "'Helvetica Neue', Arial, sans-serif",
-          color: "#f5f5f5",
-        }}
-      >
-        <h1
+      <Router>
+        <div
           style={{
-            textAlign: "center",
-            fontSize: "36px",
-            fontWeight: "200",
-            letterSpacing: "3px",
-            marginBottom: "40px",
-            color: "#ffffff",
-            textTransform: "uppercase",
+            backgroundColor: "#121212",
+            minHeight: "100vh",
+            padding: "40px 20px",
+            fontFamily: "'Helvetica Neue', Arial, sans-serif",
+            color: "#f5f5f5",
           }}
         >
-          ギター練習アプリ
-        </h1>
-        <Metronome />
-        <ChordDisplay />
-      </div>
+          <h1
+            style={{
+              textAlign: "center",
+              fontSize: "36px",
+              fontWeight: "200",
+              letterSpacing: "3px",
+              marginBottom: "40px",
+              color: "#ffffff",
+              textTransform: "uppercase",
+            }}
+          >
+            ギター練習アプリ
+          </h1>
+
+          <nav style={{ textAlign: "center", marginBottom: "30px" }}>
+            <Link to="/" style={{ marginRight: "20px", color: "#90caf9" }}>
+              ランダムコード
+            </Link>
+            <Link to="/metronome" style={{ marginRight: "20px", color: "#90caf9" }}>
+              メトロノーム
+            </Link>
+            <Link to="/chords" style={{ color: "#90caf9" }}>
+              コード一覧
+            </Link>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/metronome" element={<Metronome />} />
+            <Route path="/chords" element={<ChordGallery />} />
+          </Routes>
+        </div>
+      </Router>
     </BpmProvider>
   )
 }
 
 export default App
-
---
+++ src/components/ChordGallery.js
@@ -0,0 +1,35 @@
+const chords = [
+  { name: "C", image: "C.png" },
+  { name: "G", image: "G.png" },
+  { name: "D", image: "D.png" },
+  { name: "Am", image: "Am.png" },
+  { name: "Em", image: "Em.png" },
+  { name: "F", image: "F.png" },
+  { name: "Bm", image: "Bm.png" },
+]
+
+export default function ChordGallery() {
+  return (
+    <div style={{ marginTop: "40px" }}>
+      <h2 style={{ fontSize: "24px", marginBottom: "20px" }}>コード一覧</h2>
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+        {chords.map((chord) => (
+          <div
+            key={chord.name}
+            style={{
+              width: "100px",
+              textAlign: "center",
+            }}
+          >
+            <img
+              src={chord.image}
+              alt={`${chord.name} chord`}
+              style={{ width: "100%", borderRadius: "8px" }}
+            />
+            <p>{chord.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
